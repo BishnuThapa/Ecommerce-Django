188eda56dc580bbed9530266a37a58bc8b0a4d51
Bishnu <thapabishnu20@gmail.com>
2024-02-06 18:20:56 +0545

    add usercreationform for user signup

--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .forms import UserRegisterForm
 # Create your views here.
+def register_view(request):
+    if request.method=='POST':
+        pass
+    form= UserRegisterForm()
+    context={
+        'form':form
+
+    }
+    return render(request,'userauth/sign-up.html',context)
