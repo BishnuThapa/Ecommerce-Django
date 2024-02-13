366a18cdc75c8121f0b5648adadf58f3d0435b4d
Bishnu <thapabishnu20@gmail.com>
2024-02-13 16:38:28 +0545

    retrive vendor details in product details page

--- core/context_processor.py
+++ core/context_processor.py
@@ -1,11 +1,13 @@
 
-from .models import Category, Vendor
+from .models import Category, Vendor, Address
 
 
 def default(request):
     categories = Category.objects.all()
     vendors = Vendor.objects.all()
+    address = Address.objects.get(user=request.user)
     return {
         'categories': categories,
-        'vendors': vendors
+        'vendors': vendors,
+        'address': address
     }
